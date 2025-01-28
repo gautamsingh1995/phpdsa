ec4210463b2ca501f338b404bdda2332f4926371
gautamsingh1995 <125817615+gautamsingh1995@users.noreply.github.com>
2025-01-28 15:08:32 +0000

    Second Largest Salaries

--- secondLargestElement.php
+++ secondLargestElement.php
@@ -1,5 +1,23 @@
-
 <?php
 
-echo "Try programiz.pro";
+    #Second Largest Salaries in an Array
+    function secondLargestSalaries($salaries){
+        # Remove duplicate salaries
+        $salaries = array_unique($salaries);
+
+        # Sort the array in descending order
+        rsort($salaries);
+
+        # Return the second largest element, or null if not enough values
+        if($salaries  > 2){
+            return null;  // Not enough values for second-largest
+        }
+
+        return $salaries[1];  // Second largest salary
+
+    }
+
+    $salaries = [4000, 2000, 1000, 3000, 4000];
+    echo secondLargestSalaries($salaries); // Output: 3000
+
 ?>
