456822434b066650b3fcc1dd88e3493ec6ddaa08
gautamsingh1995 <125817615+gautamsingh1995@users.noreply.github.com>
2025-01-28 15:41:13 +0000

    find the palindrome string

---
+++ isPalindrome.php
@@ -0,0 +1,27 @@
+<?php
+
+    #Function to find the isPalindrome string:
+    function isPalindrome($strs){
+        $length = strlen($strs);
+        for ($i=0; $i < $length/2 ; $i++) { 
+            
+            if($strs[$i] !== $strs[$length-1-$i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    $x = "racecar";
+    var_dump(isPalindrome($x)); // Output: bool(true)
+
+
+    #other Method
+   /* function isPalindrome($x) {
+        // Reverse the string and check if it is the same as the original
+        return $x === strrev($x);
+    }
+    */
+    
+
+?>
